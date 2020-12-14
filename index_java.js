c518d4d4e4970984ad8cad43bd183bec0d5f3682
CassandraThalia <W0449188@nscc.ca>
2020-12-14 13:58:33 -0400

    text fade index working

--- index_java.js
+++ index_java.js
@@ -1,12 +1,9 @@
 window.setTimeout(function(){
-    var body = document.getElementById("body")
-    body.classList.add("fadeOut")
     window.location.href = "about.html";
-
 }, 4000);
 
 window.setTimeout(function(){
-    var body = document.getElementById("body")
+    var body = document.getElementById("largeHeader")
     body.classList.add("fadeOut")
-}, 2000);
+}, 1000);
 
