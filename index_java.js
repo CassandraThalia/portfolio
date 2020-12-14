cc22d0766dfb7ffbcb1106aa548788a654ba2230
CassandraThalia <W0449188@nscc.ca>
2020-12-14 11:42:27 -0400

    many more changes

---
+++ index_java.js
@@ -0,0 +1,12 @@
+window.setTimeout(function(){
+    var body = document.getElementById("body")
+    body.classList.add("fadeOut")
+    window.location.href = "about.html";
+
+}, 4000);
+
+window.setTimeout(function(){
+    var body = document.getElementById("body")
+    body.classList.add("fadeOut")
+}, 2000);
+
